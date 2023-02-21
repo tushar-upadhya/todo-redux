e999067e890fefc0a2c32d92274ef5cc35e4dcf8
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-02-21 11:03:03 +0530

    ac

---
+++ src/redux/Action/action.js
@@ -0,0 +1,6 @@
+export const ADD = (items) => {
+    return {
+        type: "ADD_DATA",
+        payload: "items",
+    };
+};
---
+++ src/redux/Reducers/reducer.js
@@ -0,0 +1,13 @@
+const Initial_State = {
+    User_Data: [],
+};
+
+export const todoReducer = (state = Initial_State, action) => {
+    switch (action.type) {
+        case "ADD_DATA":
+            return {
+                ...state,
+                User_Data: [...state.User_Data, action.payload],
+            };
+    }
+};

335e6694df636127475a4ad50b12c45cce08dc4c
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-02-23 17:41:09 +0530

    re

--- src/redux/Reducers/reducer.js
+++ src/redux/Reducers/reducer.js
@@ -10,6 +10,26 @@ export const todoReducer = (state = Initial_State, action) => {
                 User_Data: [...state.User_Data, action.payload],
             };
 
+        case "RMV_DATA":
+            const dltData = state.User_Data.filter(
+                (ele, k) => k !== action.payload
+            );
+
+            return {
+                ...state,
+                User_Data: dltData,
+            };
+
+        case "UPDATE_DATA":
+            const updateData = state.User_Data.map((ele, k) =>
+                k == action.d ? action.payload : ele
+            );
+
+            return {
+                ...state,
+                User_Data: updateData,
+            };
+
         default:
             return state;
     }
